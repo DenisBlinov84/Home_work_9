7630f4d906127060ece1bf9082356976a49a0cf8
Denis Blinov <blinovda1@mail.ru>
2022-08-11 17:36:34 +0300

    add tic_tac_toe

---
+++ tic_tac_toe/setting.py
@@ -0,0 +1,5 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+app = ApplicationBuilder().token("5491980629:AAESVGJQkJ-avRaGBzO5UsSgCvpH1v32ey").build()
